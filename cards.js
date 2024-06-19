79ac746b680624dc2a127efbfc8e80b1d0cd8247
egjohnc02 <154654372+egjohnc02@users.noreply.github.com>
2024-06-19 22:08:15 +0700

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,313 @@
+const cardList =[
+    {
+        name: "2 Heart",
+        image: "Heart 2.png",
+        value: 2
+    },
+
+    {
+        name: "3 Heart",
+        image: "Heart 3.png",
+        value: 3
+    },
+    
+    {
+        name: "4 Heart",
+        image: "Heart 4.png",
+        value: 4
+    },
+    
+    {
+        name: "5 Heart",
+        image: "Heart 5.png",
+        value: 5
+    },
+    
+    {
+        name: "6 Heart",
+        image: "Heart 6.png",
+        value: 6
+    },
+    
+    {
+        name: "7 Heart",
+        image: "Heart 7.png",
+        value: 7
+    },
+    
+    {
+        name: "8 Heart",
+        image: "Heart 8.png",
+        value: 8
+    },
+    
+    {
+        name: "9 Heart",
+        image: "Heart 9.png",
+        value: 9
+    },
+    
+    {
+        name: "10 Heart",
+        image: "Heart 10.png",
+        value: 10
+    },
+    
+    {
+        name: "J Heart",
+        image: "Heart J.png",
+        value: 11
+    },
+    
+    {
+        name: "Q Heart",
+        image: "Heart Q.png",
+        value: 12
+    },
+    
+    {
+        name: "K Heart",
+        image: "Heart K.png",
+        value: 13
+    },
+
+    {
+        name: "A Heart",
+        image: "Heart A.png",
+        value: [11, 1]
+    },
+    
+    {
+        name: "2 Diamond",
+        image: "Diamond 2.png",
+        value: 2
+    },
+    
+    {
+        name: "3 Diamond",
+        image: "Diamond 3.png",
+        value: 3
+    },
+    
+    {
+        name: "4 Diamond",
+        image: "Diamond 4.png",
+        value: 4
+    },
+    
+    {
+        name: "5 Diamond",
+        image: "Diamond 5.png",
+        value: 5
+    },
+
+    {
+        name: "6 Diamond",
+        image: "Diamond 6 .png",
+        value: 6
+    },
+    
+    {
+        name: "7 Diamond",
+        image: "Diamond 7.png",
+        value: 7
+    },
+    
+    {
+        name: "8 Diamond",
+        image: "Diamond 8.png",
+        value: 8
+    },
+    
+    {
+        name: "9 Diamond",
+        image: "Diamond 9.png",
+        value: 9
+    },
+    
+    {
+        name: "10 Diamond",
+        image: "Diamond 10.png",
+        value: 10
+    },
+    
+    {
+        name: "J Diamond",
+        image: "Diamond j.png",
+        value: 11
+    },
+    
+    {
+        name: "Q Diamond",
+        image: "Diamond Q.png",
+        value: 12
+    },
+    
+    {
+        name: "K Diamond",
+        image: "Diamond K.png",
+        value: 13
+    },
+    
+    {
+        name: "A Diamond",
+        image: "Diamond A.png",
+        value: [11, 1]
+    },
+    
+    {
+        name: "2 Shrimp",
+        image: "Shrimp 2.png",
+        value: 2
+    },
+    
+    {
+        name: "3 Shrimp",
+        image: "Shrimp 3.png",
+        value: 3
+    },
+    
+    {
+        name: "4 Shrimp",
+        image: "Shrimp 4.png",
+        value: 4
+    },
+    
+    {
+        name: "5 Shrimp",
+        image: "Shrimp 5.png",
+        value: 5
+    },
+
+    {
+        name: "6 Shrimp",
+        image: "Shrimp 6 .png",
+        value: 6
+    },
+    
+    {
+        name: "7 Shrimp",
+        image: "Shrimp 7.png",
+        value: 7
+    },
+    
+    {
+        name: "8 Shrimp",
+        image: "Shrimp 8.png",
+        value: 8
+    },
+    
+    {
+        name: "9 Shrimp",
+        image: "Shrimp 9.png",
+        value: 9
+    },
+    
+    {
+        name: "10 Shrimp",
+        image: "Shrimp 10.png",
+        value: 10
+    },
+    
+    {
+        name: "J Shrimp",
+        image: "Shrimp j.png",
+        value: 11
+    },
+    
+    {
+        name: "Q Shrimp",
+        image: "Shrimp Q.png",
+        value: 12
+    },
+    
+    {
+        name: "K Shrimp",
+        image: "Shrimp K.png",
+        value: 13
+    },
+    
+    {
+        name: "A Shrimp",
+        image: "Shrimp A.png",
+        value: [11, 1]
+    },
+    
+    {
+        name: "2 Spade",
+        image: "Spade 2.png",
+        value: 2
+    },
+    
+    {
+        name: "3 Spade",
+        image: "Spade 3.png",
+        value: 3
+    },
+    
+    {
+        name: "4 Spade",
+        image: "Spade 4.png",
+        value: 4
+    },
+    
+    {
+        name: "5 Spade",
+        image: "Spade 5.png",
+        value: 5
+    },
+
+    {
+        name: "6 Spade",
+        image: "Spade 6 .png",
+        value: 6
+    },
+    
+    {
+        name: "7 Spade",
+        image: "Spade 7.png",
+        value: 7
+    },
+    
+    {
+        name: "8 Spade",
+        image: "Spade 8.png",
+        value: 8
+    },
+    
+    {
+        name: "9 Spade",
+        image: "Spade 9.png",
+        value: 9
+    },
+    
+    {
+        name: "10 Spade",
+        image: "Spade 10.png",
+        value: 10
+    },
+    
+    {
+        name: "J Spade",
+        image: "Spade j.png",
+        value: 11
+    },
+    
+    {
+        name: "Q Spade",
+        image: "Spade Q.png",
+        value: 12
+    },
+    
+    {
+        name: "K Spade",
+        image: "Spade K.png",
+        value: 13
+    },
+    
+    {
+        name: "A Spade",
+        image: "Spade A.png",
+        value: [11, 1]
+    },Spade
+]
